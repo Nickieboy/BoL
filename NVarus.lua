7a6891ce82773fb81ca922852fea10f7fdb09b6e
Nickieboy <Masterboy520@hotmail.com>
2014-11-28 14:40:11 +0100

    NVarus

--- NVarus.lua
+++ NVarus.lua
@@ -4,12 +4,41 @@
 
 if myHero.charName ~= "Varus" then return end
 
--- Download information
+-- Download script
+local version = 1.00
+local author = "Nickieboy"
+local SCRIPT_NAME = "NVarus"
+local AUTOUPDATE = true
+local UPDATE_HOST = "raw.github.com"
+local UPDATE_PATH = "/Nickieboy/BoL/master/NVarus.lua".."?rand="..math.random(1,10000)
+local UPDATE_FILE_PATH = SCRIPT_PATH..GetCurrentEnv().FILE_NAME
+local UPDATE_URL = "https://"..UPDATE_HOST..UPDATE_PATH
 
+function AutoupdaterMsg(msg) print("<font color=\"#6699ff\"><b>NVarus:</b></font> <font color=\"#FFFFFF\">"..msg..".</font>") end
+if AUTOUPDATE then
+	local ServerData = GetWebResult(UPDATE_HOST, "/Nickieboy/BoL/master/version/NVarus.version")
+	if ServerData then
+		ServerVersion = type(tonumber(ServerData)) == "number" and tonumber(ServerData) or nil
+		if ServerVersion then
+			if tonumber(version) < ServerVersion then
+				AutoupdaterMsg("New version available "..ServerVersion)
+				AutoupdaterMsg("Updating, please don't press F9")
+				DelayAction(function() DownloadFile(UPDATE_URL, UPDATE_FILE_PATH, function () AutoupdaterMsg("Successfully updated. ("..version.." => "..ServerVersion.."), press F9 twice to load the updated version.") end) end, 3)
+			else
+				AutoupdaterMsg("You have got the latest version ("..ServerVersion..") of NVarus by " .. author)
+			end
+		end
+	else
+		AutoupdaterMsg("Error downloading version info")
+	end
+end
+
+
+-- Download Libraries
 local REQUIRED_LIBS = {
-	["SOW"] = "https://raw.githubusercontent.com/Hellsing/BoL/master/common/SOW.lua",
+	["SxOrbWalk"] = "https://raw.githubusercontent.com/Superx321/BoL/master/common/SxOrbWalk.lua",
 	["VPrediction"] = "https://raw.githubusercontent.com/Hellsing/BoL/master/common/VPrediction.lua",
-	["SourceLib"] = "https://raw.github.com/TheRealSource/public/master/common/SourceLib.lua"
+	["SourceLib"] = "https://raw.githubusercontent.com/Dienofail/BoL/master/common/SourceLib.lua"
 }
 local DOWNLOADING_LIBS, DOWNLOAD_COUNT = false, 0
 
@@ -32,8 +61,6 @@ for DOWNLOAD_LIB_NAME, DOWNLOAD_LIB_URL in pairs(REQUIRED_LIBS) do
 end
 
 -- Local Variables
-local version = 1.00
-local author = "Nickieboy"
 local Qspell = {name = "Piercing Arrow", duration = 1.25, speed = 1900, width = 60, delay = 0}
 local Qrangemin = 850
 local Qrangemax = 1800
@@ -47,20 +74,26 @@ local Rready = (myHero:CanUseSpell(_Q) == READY)
 local Qtarget, Etarget, Rtarget, AAtarget = nil
 local EnemyMinions = minionManager(MINION_ENEMY, 625, myHero, MINION_SORT_HEALTH_ASC)
 local farmMinions = 0
+local healPosition = nil
 
 local myPos = myHero.pos
 local targetPos = nil
 local newPos = nil
 local newTargetPos = nil
+local xFurther = 0
 
 
 -- Load when game loads
 function OnLoad()
 	VP = VPrediction()
-	SOW = SOW(VP)
-	WPManager = WayPointManager()
+	SxOrb = SxOrbWalk(VP)
     print("NVarus succesfully loaded")
 	STS = SimpleTS(STS_LESS_CAST_PHYSICAL)
+	DMG = DamageLib()
+	--Register E damage
+	DMG:RegisterDamageSource(_E, _PHYSICAL, 30, 65, _PHYSICAL, _AD, 0.6, function() return (myHero:CanUseSpell(_E) == READY) end)
+	DMG:RegisterDamageSource(_R, _MAGIC, 50, 100, _MAGIC, _AP, 1.0, function() return (myHero:CanUseSpell(_R) == READY) end)
+
 
 	Menu()
 
@@ -80,6 +113,12 @@ function OnLoad()
 	R = Spell(_R, Rspell.range, false)
 	R:SetSkillshot(VP, SKILLSHOT_LINEAR, Rspell.width, R.delay, Rspell.speed, false)
 	R:SetAOE(true)
+
+	if myHero:GetSpellData(SUMMONER_1).name:find("summonerheal") then 
+		healPosition = SUMMONER_1
+    elseif myHero:GetSpellData(SUMMONER_2).name:find("summonerheal") then 
+    	healPosition = SUMMONER_2
+    end 
 end
 
 -- Checks frequently
@@ -89,6 +128,10 @@ function OnTick()
 	end 
 	if Menu.combo.combo then
 		Combo()
+	end 
+
+	if Menu.killsteal.killsteal then
+		KillSteal()
 	end 
 
 	if Menu.misc.skinChanger.skinChanger and skinChanged() then
@@ -103,15 +146,15 @@ function OnTick()
 	if Menu.laneclear.laneclear then 
 		LaneClear()
 	end 
+
+	if Menu.misc.autoheal.useHeal and healPosition ~= nil then
+		UseSummoner()
+	end 
 end
 
 -- Checks same with FPS
 function OnDraw()
 	if Menu.drawings.draw then
-		if Menu.drawings.drawAA then
-			DrawCircle(myHero.x, myHero.y, myHero.z, AA, 0x111111)
-		end 
-
 		if Menu.drawings.drawQ then
 			DrawCircle(myHero.x, myHero.y, myHero.z, 1200, 0x111111)
 		end
@@ -123,7 +166,7 @@ function OnDraw()
 		if Menu.drawings.drawR then
 			DrawCircle(myHero.x, myHero.y, myHero.z, Rspell.range, 0x111111)
 		end
-
+		STS:OnDraw()
 	end 
 end
 
@@ -132,39 +175,41 @@ function Combo()
 	Qtarget = STS:GetTarget(Qrangemax)
 	Etarget = STS:GetTarget(Espell.range)
 	Rtarget = STS:GetTarget(Rspell.range)
-	if Menu.combo then
-		if Menu.combo.comboQ then
-			if Qtarget and Qready then
-				if Q:IsCharging() then
+	if Menu.combo.combo then
+		if Menu.combo.comboQ.comboQ then
+			if Menu.combo.comboQ.comboQinstant then
+				if Qtarget and Qready then
+					if Q:IsCharging() then
 
-					local castPosition, hitChance, nTargets = Q:GetPrediction(Qtarget)
-					if Q.range ~= Qrangemax and GetDistanceSqr(castPosition) < math.pow(Q.range - 200, 2) then
-						Send2ndQPacket(castPosition)
+						local castPosition, hitChance, nTargets = Q:GetPrediction(Qtarget)
+						if Q.range ~= Qrangemax and GetDistanceSqr(castPosition) < math.pow(Q.range - 200, 2) then
+							Send2ndQPacket(castPosition)
+						end 
+					else
+						Q:Charge()
 					end 
-				else
-					Q:Charge()
-				end 
-				
-			end
-		end 
-
-		if Menu.combo.comboE and not Menu.combo.comboEFurther then
-			if Etarget and Eready  then
-				E:Cast(Etarget.x, Etarget.z)
+					
+				end
 			end 
 		end 
-		if Menu.combo.comboEFurther and not Menu.combo.comboE then
-			EnemyMovingFurtherAway(E, 760, Etarget)
-			--if IsRunningAway(Etarget) then
-			--	if Eready then
-			--		E:Cast(Etarget)
-			--	end 
-			--end 
-		end 
 
-		if Menu.combo.comboR then
+		if Menu.combo.comboE.comboE then
+			if Etarget and Eready then
+				if Menu.combo.comboE.comboEinstant then
+					E:Cast(Etarget.x, Etarget.z)
+				end 
+				if Menu.combo.comboE.comboEFurther then
+					EnemyMovingFurtherAwayCastE(760, Etarget)
+				end 
+			end 
+		end 
+		
+
+		if Menu.combo.comboR.comboR then
 			if Rtarget and Rready then
-				R:Cast(Rtarget)
+				if Menu.combo.comboR.comboRinstant then
+					R:Cast(Rtarget)
+				end 
 			end 
 		end 
 	end
@@ -209,12 +254,36 @@ function LaneClear()
 		
 			if Menu.laneclear.laneclearE and Eready then
 				local farmPos2, farmHit2 = GetBestCircularFarmPosition(Espell.range, 50, EnemyMinions.objects)
-				if farmHit2 >= 3 then
+				if farmPos2 then
 					E:Cast(farmPos2.x, farmPos2.z)
 				end 
 			end 
 		end 
 	end 
+end 
+
+
+-- KillSteal
+function KillSteal()
+	for i, enemy in ipairs(GetEnemyHeroes()) do
+		if Menu.killsteal.killstealAA then
+			if ValidTarget(enemy, AA) then
+				if DMG:IsKillable(enemy, {_AA}) then
+					SxOrb:ForceTarget(enemy)
+					SxOrb:AttackSelectedTarget()
+				end 
+			end 
+		end 
+
+		if Menu.killsteal.killstealE then
+			if (ValidTarget(enemy, Espell.range)) then
+				if DMG:IsKillable(enemy, {_E}) then
+					E:Cast(enemy.x, enemy.z)
+				end 
+			end 
+		end 
+	end 
+
 end 
 
 function Send2ndQPacket(to)
@@ -230,24 +299,33 @@ function Send2ndQPacket(to)
 	SendPacket(packet)
 end
 
-function EnemyMovingFurtherAway(spell, range, target)
-	print("Entering function")
+-- Credits to Devn for showing DelayAction
+function EnemyMovingFurtherAwayCastE(range, target)
 	if (target and ValidTarget(target, range)) then
-		print("Target Valid")
 		local distance = GetDistance(target, myHero)
-		print("Distance " .. distance)
-		DelayAction(function()
-			print("Entering delay")
+		DelayAction(function(range, target)
 			if (ValidTarget(target, range)) then
-				print("Target still valid")
 				local newDistance = GetDistance(target, myHero)
-				if (newDistance > distance and myHero:CanUseSpell(_E) == READY) then
+				if (newDistance > distance) then
+					xFurther = xFurther + 1;
+				else 
+					xFurther = xFurther - 1;
+				end 
+				if (xFurther < 0) then
+					xFurther = 0
+				end 
+				if (xFurther >= 5) then
+					xFurther = 0
+				end 
+				if (myHero:CanUseSpell(_E) == READY) and xFurther >= 4 and ValidTarget(target, range)  then
 					E:Cast(target.x, target.z)
 				end
 			end
-		end, 1)
+		end, 1, {range, target})
 	end
 end
+
+-- Works, but gives error with LIB_PATH
 --[[function EnemyMovingFurtherAway(range, target) 
 	print("Going in function")
 	myPos = myHero.pos
@@ -284,14 +362,8 @@ if (ValidTarget(target, range)) then --Check to make sure target is valid becaus
 			end
 end 
 ]]
-function IsRunningAway(unit)
-	local waypoints = WPManager:GetWayPoints(unit)
-	if (waypoints and #waypoints > 0) then
-		return (GetDistance(waypoints[0], myHero) > GetDistance(unit, myHero))
-	else
-		return false
-	end
-end
+
+
 
 function HaveBuff(unit,buffname)
 	local result = false
@@ -317,6 +389,32 @@ function castStunInterruptable(unit, spell)
 	if Rready and GetDistance(unit) <= R.range then
 		R:Cast(unit)
 	end
+end 
+
+function UseSummoner()
+	health = myHero.health
+	maxHealth = myHero.maxHealth
+
+	if (myHero:CanUseSpell(healPosition) == READY) then
+		if ((health / maxHealth) <= Menu.misc.autoheal.amountOfHealth)) then
+			CastSpell(healPosition)
+		end 
+	end 
+
+	if Menu.misc.autoheal.amountOfHealth then
+		for i, teammate in ipairs(GetAllyHeroes()) do
+			if GetDistance(teammate, myHero) <= 700 then
+				health = teammate.health
+				maxHealth = teammate.maxHealth
+
+				if ((health / maxHealth) <= Menu.misc.autoheal.amountOfHealth) then
+					if (myHero:CanUseSpell(healPosition) == READY) then
+						CastSpell(healPosition)
+					end 
+				end 
+			end 
+		end 
+	end 
 end 
 
 function DrinkPotions()
@@ -384,18 +482,30 @@ function Menu()
 
 	 -- Combo
 	Menu:addSubMenu("Combo", "combo")
+
  	Menu.combo:addParam("combo", "Combo", SCRIPT_PARAM_ONKEYDOWN, false, 32)
- 	Menu.combo:addParam("comboQ", "Use " .. Qspell.name .. " (Q)", SCRIPT_PARAM_ONOFF, true)
- 	Menu.combo:addParam("comboE", "Use " .. Espell.name .. " (E) instantly", SCRIPT_PARAM_ONOFF, false)
- 	Menu.combo:addParam("comboEFurther", "Use " .. Espell.name .. " (E) not instantly", SCRIPT_PARAM_ONOFF, true)
- 	Menu.combo:addParam("EFurther", "E Further", SCRIPT_PARAM_INFO, "^ Only use E when enemy goes further away")
- 	Menu.combo:addParam("comboR", "Use " .. Rspell.name .. " (R)", SCRIPT_PARAM_ONOFF, true)
+ 	
+ 		-- Use Q in combo
+ 	Menu.combo:addSubMenu(Qspell.name .. " (Q)", "comboQ")
+ 	Menu.combo.comboQ:addParam("comboQ", "Use " .. Qspell.name .. " (Q)", SCRIPT_PARAM_ONOFF, true)
+ 	--Menu.combo.comboQ:addParam("comboQmax", "Cast in full range", SCRIPT_PARAM_ONOFF, false)
+ 	Menu.combo.comboQ:addParam("comboQinstant", "Cast Q immediately", SCRIPT_PARAM_ONOFF, true)
+
+ 		-- Use E in combo
+ 	Menu.combo:addSubMenu(Espell.name .. " (E)", "comboE")
+ 	Menu.combo.comboE:addParam("comboE", "Use " .. Espell.name .. " (E)", SCRIPT_PARAM_ONOFF, true)
+ 	Menu.combo.comboE:addParam("comboEinstant", "Cast instantly", SCRIPT_PARAM_ONOFF, false)
+ 	Menu.combo.comboE:addParam("comboEFurther", "Use if Enemy runs away", SCRIPT_PARAM_ONOFF, true)
+ 	
+ 		-- Use R in combo
+ 	Menu.combo:addSubMenu(Rspell.name .. " (R)", "comboR")
+ 	Menu.combo.comboR:addParam("comboR", "Use " .. Rspell.name .. " (R)", SCRIPT_PARAM_ONOFF, true)
+ 	Menu.combo.comboR:addParam("comboRinstant", "Use instantly", SCRIPT_PARAM_ONOFF, true)
 
  	 -- Harass
 	Menu:addSubMenu("Harass", "harass")
  	Menu.harass:addParam("harass", "Harass", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("T"))
  	Menu.harass:addParam("harassQ", "Use " .. Qspell.name .. " (Q)", SCRIPT_PARAM_ONOFF, true)
- 	Menu.harass:addParam("harassE", "Use " .. Espell.name .. " (E)", SCRIPT_PARAM_ONOFF, true)
 
  	Menu:addSubMenu("LaneClear", "laneclear")
  	Menu.laneclear:addParam("laneclear", "Laneclear", SCRIPT_PARAM_ONKEYDOWN, false, string.byte("U"))
@@ -405,20 +515,17 @@ function Menu()
  	 -- Combo
 	Menu:addSubMenu("KillSteal", "killsteal")
  	Menu.killsteal:addParam("killsteal", "KillSteal", SCRIPT_PARAM_ONOFF, false)
- 	Menu.killsteal:addParam("killstealQ", "Use " .. Qspell.name, SCRIPT_PARAM_ONOFF, true)
- 	Menu.killsteal:addParam("killstealE", "Use " .. Espell.name, SCRIPT_PARAM_ONOFF, true)
-
- 	--Orbwalker
- 	Menu:addSubMenu("OrbWalker", "orbwalker")
- 	SOW:LoadToMenu(Menu.orbwalker)
+ 	Menu.killsteal:addParam("killstealAA", "Use AA", SCRIPT_PARAM_ONOFF, false)
+ 	Menu.killsteal:addParam("killstealE", "Use " .. Espell.name .. " (E)", SCRIPT_PARAM_ONOFF, false)
 
  	--Drawings
  	Menu:addSubMenu("Drawings", "drawings")
- 	Menu.drawings:addParam("draw", "Use Drawings", SCRIPT_PARAM_ONOFF, false)
- 	Menu.drawings:addParam("drawAA", "Draw Attack Speed", SCRIPT_PARAM_ONOFF, false)
- 	Menu.drawings:addParam("drawQ", "Draw " .. Qspell.name .. " (Q)", SCRIPT_PARAM_ONOFF, false)
- 	Menu.drawings:addParam("drawE", "Draw " .. Espell.name .. " (E)", SCRIPT_PARAM_ONOFF, false)
- 	Menu.drawings:addParam("drawR", "Draw " .. Rspell.name .. " (R)", SCRIPT_PARAM_ONOFF, false)
+ 	Menu.drawings:addParam("draw", "Use Drawings", SCRIPT_PARAM_ONOFF, true)
+ 	Menu.drawings:addParam("drawQ", "Draw " .. Qspell.name .. " (Q)", SCRIPT_PARAM_ONOFF, true)
+ 	Menu.drawings:addParam("drawE", "Draw " .. Espell.name .. " (E)", SCRIPT_PARAM_ONOFF, true)
+ 	Menu.drawings:addParam("drawR", "Draw " .. Rspell.name .. " (R)", SCRIPT_PARAM_ONOFF, true)
+ 	DMG:AddToMenu(Menu.drawings, {_AA, _AA, _AA, _E, _R})
+ 	Menu.drawings:addParam("damageAfter", "Damage after", SCRIPT_PARAM_INFO, "3 AA, E and R")
 
  	--Misc
  	Menu:addSubMenu("Misc", "misc")
@@ -427,26 +534,48 @@ function Menu()
  	Menu.misc.autopotions:addParam("usePotion", "Use Potions Automatically", SCRIPT_PARAM_ONOFF, false)
  	Menu.misc.autopotions:addParam("health", "Health under %", SCRIPT_PARAM_SLICE, 0.25, 0, 1, 2)
  	Menu.misc.autopotions:addParam("mana", "Mana under %", SCRIPT_PARAM_SLICE, 0.25, 0, 1, 2)
- 	
- 	Menu.misc:addSubMenu("Skin Changer", "skinChanger")
- 	Menu.misc.skinChanger:addParam("skinChanger", "Change Skin (VIP", SCRIPT_PARAM_ONOFF, false)
- 	Menu.misc.skinChanger:addParam("skinChangerSlice", "Change Skin", SCRIPT_PARAM_SLICE, 1, 1, 4, 0)
+ 	if healPosition ~= nil then
+	Menu.misc:addSubMenu("Auto Heal", "autoheal")
+	Menu.misc.autoheal:addParam("useHeal", "Use Summoner Heal", SCRIPT_PARAM_ONOFF, false)
+	Menu.misc.autoheal:addParam("amountOfHealth", "Under % of health", SCRIPT_PARAM_SLICE, 0, 0, 1, 2)
+	Menu.misc.autoheal:addParam("helpHeal", "Use Heal to save teammates", SCRIPT_PARAM_ONOFF, false)
+	end 
+
+	Menu.misc:addSubMenu("Skin Changer", "skinChanger")
+	Menu.misc.skinChanger:addParam("skinChanger", "Change Skin (VIP)", SCRIPT_PARAM_ONOFF, false)
+	Menu.misc.skinChanger:addParam("skinChangerSlice", "Change Skin", SCRIPT_PARAM_SLICE, 1, 1, 4, 0)
 
  	Menu.misc:addSubMenu("Gapcloser", "gc")
 	AntiGapcloser(Menu.misc.gc, castStunGapClosing)
 	Menu.misc:addSubMenu("Interrupter", "ai")
 	Interrupter(Menu.misc.ai, castStunInterruptable)
 
-
 	Menu:addSubMenu("Target Selector", "sts")
 	STS:AddToMenu(Menu.sts)
 
+	--Orbwalker
+ 	Menu:addSubMenu("OrbWalker", "orbwalker")
+ 	SxOrb:LoadToMenu(Menu.orbwalker)
 
+
+	-- Permashow
   	Menu.combo:permaShow("combo")
   	Menu.harass:permaShow("harass")
   	Menu.killsteal:permaShow("killsteal")
-
+  	Menu.drawings:permaShow("draw")
+  	Menu.misc:permaShow("gc")
+  	Menu.misc:permaShow("ai")
 end
+
+function CheckMenu()
+	if Menu.combo.comboE.comboEFurther then
+		Menu.combo.comboE.comboEinstant = false
+	end 
+
+	if Menu.combo.comboE.comboEinstant then
+		Menu.combo.comboE.comboEFurther = false
+	end 
+end 
 
 
 
